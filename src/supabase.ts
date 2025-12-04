66148a124e96cab023afb52fe330a2323c17ff9c
yesmine96 <yesmine.ghorbel96@gmail.com>
2025-12-04 11:33:12 +0100

    feat: update supabase configuration to use environment variables for URL and anon key

--- src/supabase.ts
+++ src/supabase.ts
@@ -2,8 +2,8 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 import 'react-native-url-polyfill/auto';
 
-const supabaseUrl = 'https://fowtciltrjfesbcguzuy.supabase.co';
-const supabaseAnonKey = 'sb_publishable_IcyDEcKS79kAKlbYgQ3GMw_enHOjOX7';
+const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
